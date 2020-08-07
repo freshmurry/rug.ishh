d694b1f2d73c541b2a27f57ea24db2ea3281bcdc
Lawrence <lawrencemurry@yahoo.com>
2020-08-07 03:05:43 +0000

    Fixed typo in file name and table name

--- db/schema.rb
+++ db/schema.rb
@@ -136,6 +136,7 @@ ActiveRecord::Schema.define(version: 20180109174910) do
     t.datetime "locked_at"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "fullname"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
